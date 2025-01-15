300cf29a829db228c25622ae91e1bf8090fe88bc
LukeChamberlain <LukeChamberlain64@gmail.com>
2025-01-15 14:37:11 -0700

    Added the class to check if a move can be made but it isn't finished

--- shared/src/main/java/chess/calculateMoves/BishopMoves.java
+++ shared/src/main/java/chess/calculateMoves/BishopMoves.java
@@ -3,7 +3,21 @@ package chess.calculateMoves;
 import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPiece;
+import chess.ChessPosition;
+import chess.ChessGame;
+import java.util.HashSet;
 
 public class BishopMoves {
-    
+    public static HashSet<ChessMove> calculateMoves(ChessBoard board, ChessPosition position, ChessGame.TeamColor color) {
+        HashSet<ChessMove> possibleMoves = new HashSet<>();
+        int row = position.getRow();
+        int col = position.getColumn();
+
+        CheckMove.upAndLeft(board, position, color, row, col, possibleMoves);
+        CheckMove.upAndRight(board, position, color, row, col, possibleMoves);
+        CheckMove.downAndLeft(board, position, color, row, col, possibleMoves);
+        CheckMove.downAndRight(board, position, color, row, col, possibleMoves);
+
+        return possibleMoves;
+    }
 }
