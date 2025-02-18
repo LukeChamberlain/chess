4d8ab4c95c7dd0f927d1fa345d85a8cc022cc2e1
LukeChamberlain <LukeChamberlain64@gmail.com>
2025-02-18 15:46:36 -0700

    started creating the interface

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,9 +1,14 @@
 package server;
 
 import spark.*;
+import com.google.gson.Gson;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Server {
-
+    public static Set<String> tokens = new HashSet<>();
+    public static Gson gson = new Gson();
+    private static final UserService userService = new UserReg();
     public static void main(String[] args) {
         Server server = new Server();
         server.run(4567);
@@ -11,48 +16,26 @@ public class Server {
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
-
         Spark.staticFiles.location("web");
 
-        // Spark.before((request, response) -> { 
-        //     boolean authenticated = false; 
+        Spark.before((request, response) -> { 
+            String path = request.pathInfo();
+            if (!path.equals("/user")) {
+                String authToken = request.headers("Authorization");
+                boolean authenticated = authToken != null && tokens.contains(authToken);
+                if (!authenticated) { 
+                Spark.halt(401, "You are not welcome here"); 
+                } 
+            }
+        });
           
-        //     // ... check if authenticated 
-          
-        //     if (!authenticated) { 
-        //       Spark.halt(401, "You are not welcome here"); 
-        //     } 
-        //   });
-          
+        Spark.post("/user", (request, response) -> userService.register(request, response));
 
-        Spark.get("/", (request, response) -> {
-            response.status(200);
-            response.type("text/plain");
-            response.header("CS240", "Awesome!");
-            response.body("Hello, World!");
-            return response.body();
+        Spark.exception(Exception.class, (exception, req, res) -> {
+            res.status(500);
+            res.body("Internal Server Error: " + exception.getMessage());
         });
-        Spark.post("/", (request, response) -> {
-            //create something
-            return "post";
-        });
-        Spark.put("/", (request, response) -> {
-            //Update something
-            return "put";
-        });
-        Spark.delete("/", (request, response) -> {
-            //Delete something
-            return "delete";
-        });
-        
-        Spark.init();
 
-        Spark.awaitInitialization();
-        return Spark.port();
-    }
-
-    public void stop() {
-        Spark.stop();
-        Spark.awaitStop();
+        return desiredPort;
     }
 }
